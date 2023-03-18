af54c4736adede411c9f07e6be12d186686b79e0
André Luiz Tiago Soares <asoares@covidclinic.org>
2023-03-18 00:34:57 -0300

    Found solution to get yt video transcriptions

--- src/quickstart.py
+++ src/quickstart.py
@@ -4,40 +4,12 @@
 # See instructions for running these code samples locally:
 # https://developers.google.com/explorer-help/code-samples#python
 
-import os
+from youtube_transcript_api import YouTubeTranscriptApi
 
-from google_auth_oauthlib.flow import InstalledAppFlow
-import googleapiclient.discovery
-import googleapiclient.errors
-
-SCOPES = ["https://www.googleapis.com/auth/youtube.readonly"]
 
 def main():
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-
-    api_service_name = "youtube"
-    api_version = "v3"
-    client_secrets_file = "credentials.json"
-
-    # Get credentials and create an API client
-    flow = InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file,
-    scopes=SCOPES)
-
-    flow.run_local_server()
-
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, credentials=flow.credentials)
-
-    request = youtube.channels().list(
-        part="snippet,contentDetails,statistics",
-        id="UC_x5XG1OV2P6uZZ5FSM9Ttw"
-    )
-    response = request.execute()
-
-    print(response)
+    transcript = YouTubeTranscriptApi.get_transcript('WKbWOZAnfXM')
+    print(transcript)
 
 if __name__ == "__main__":
     main()
