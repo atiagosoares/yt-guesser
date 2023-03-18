1b8247da7369b0b1afc43fd4916fa4babdfdc25c
atiagosoares <atiagosoares@gmail.com>
2023-03-18 10:49:38 -0300

    Sample script to fetch transcript of a video

---
+++ src/get_caption.py
@@ -0,0 +1,10 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+import json
+
+def main():
+    video_id = "TJ2ifmkGGus"
+    # Fetch transcript
+    transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    # Save it to file
+    with open('data/sample_transcript.py', 'w') as f:
+        json.dump(transcript, f)
