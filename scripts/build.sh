6a9af821a9dd7376bf1de31d1bfc24fd250ad2b1
atiagosoares <atiagosoares@gmail.com>
2023-03-30 23:15:25 -0300

    Triyng to fix build

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,6 @@ rm -rf build/process_transcripts/*
 # Install dependencies
 pip install -r src/process_transcripts/requirements.txt -t build/process_transcripts
 # Move source code
+# Activate the environment and install nltk punkt
+python -m nltk.downloader punkt -d build/process_transcripts/nltk_data
 cp -r src/process_transcripts/* build/process_transcripts
