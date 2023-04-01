d5b542a1603c589a75958039162e6afd19d28992
atiagosoares <atiagosoares@gmail.com>
2023-04-01 15:14:52 -0300

    Added a bunch of tests for the enricher and interpolatier classes

--- tests/process_transcripts/test_enricher.py
+++ tests/process_transcripts/test_enricher.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append('src/process_transcripts')
 
 import pytest
-from enricher import OpenAIChatEnricher
+from enricher import OpenAIChatEnricher, PositionInterpolator
 import os
 import json
 
@@ -15,7 +15,7 @@ transcript_json = '''
     {"text": "so how many types of nerds are there out", "start": 0.01, "duration": 4.92},
     {"text": "there so I have a list you got developer", "start": 3.01, "duration": 5.52},
     {"text": "nerds you got audiophile nerds you have", "start": 4.93, "duration": 6.9},
-    {"text": "gaming nerds you have bang Olu some", "start": 8.53, "duration": 5.04},
+    {"text": "gaming nerds you have bang me", "start": 8.53, "duration": 5.04},
     {"text": "nerds those are a very specialized nerd", "start": 11.83, "duration": 4.86},
     {"text": "case anyway you got Tech Hardware nerds", "start": 13.57, "duration": 6.18},
     {"text": "you also have Star Wars nerds so what", "start": 16.69, "duration": 5.759},
@@ -28,21 +28,150 @@ transcript_json = '''
 '''
 transcript = json.loads(transcript_json)
 
-# Initialize object for testing
-def test_concat_transcript():
-    concat_transcript = enricher._concat_transcript(transcript)
-    want = '''
-so how many types of nerds are there out
-there so I have a list you got developer
-nerds you got audiophile nerds you have
-gaming nerds you have bang Olu some
-nerds those are a very specialized nerd
-case anyway you got Tech Hardware nerds
-you also have Star Wars nerds so what
-other types of nerds are out there try
-to think about the major branches of
-nerdness not the specialized super nerds
-but the major branches of nerdness let
-me know in the comments below
+# ------------------------------------------------------- COUNT TOKENS -------------------------------------------------------
+def test_count_tokens():
+    # Shoudld return something like this:
+    assert enricher._count_tokens(transcript[0]['text']) == 9
+    assert enricher._count_tokens(transcript[1]['text']) == 9
+    assert enricher._count_tokens(transcript[2]['text']) == 7
+
+def test_map_count_tokens():
+    # Should return something like this:
+    captions = [caption['text'] for caption in transcript]
+    token_counts = captions.map(enricher._count_tokens)
+    assert token_counts == [len(caption.split(' ')) for caption in captions]
+
+# ------------------------------------------------------- GET POS TIMESTAMPS --------------------------------------------------
 '''
-    assert concat_transcript == want
+Assumptions about the transcript:
+- 'start' is float and never null
+- 'text' is string and never null
+- There will be at least two captions
+'''
+def test_get_pos_timestamps_empty():
+    # Edge case: empty caption
+    # Should never happen, but behavior is defined, just in case
+    captions = []
+    timestamps = enricher._get_pos_timestamps(captions)
+    assert timestamps == []
+
+def test_get_pos_timestamps_single():
+    # Single caption
+    # Should never happen, but behavior is defined, just in case
+    captions = [{'start': 0.00, 'text': 'foo'}]
+    timestamps = enricher._get_pos_timestamps(captions)
+    assert timestamps == [(0, 0.00)]
+
+def test_get_pos_timestamps_multiple():
+    # Multiple captions
+    captions = [
+        {'start': 0.00, 'text': 'foo'},
+        {'start': 5.00, 'text': 'bar'},
+        {'start': 10.00, 'text': 'baz'}
+    ]
+    timestamps = enricher._get_pos_timestamps(captions)
+    assert timestamps == [(0, 0.00), (4, 5.00), (8, 10.00)]
+
+# ------------------------------------------------------- CREATE CHUNKS -------------------------------------------------------
+def test_create_chunks_empty_list():
+    # Edge case: empty caption
+    captions = []
+    chunks = enricher._create_chunks(captions)
+    assert chunks == [] # Empty list
+
+def test_create_chunks_single():
+    # Should split the strings in chunks of >=1500 tokens
+    captions = [
+        ('foo', 1000),
+        ('bar', 499),
+    ]
+    chunks = enricher._create_chunks(captions)
+    assert chunks == [['foo', 'bar']] # Single chunk
+
+    captions = [
+        ('foo', 1000),
+        ('bar', 500)
+    ]
+    chunks = enricher._create_chunks(captions)
+    assert chunks == [['foo', 'bar']] # Single chunk
+
+def test_create_chunks_multiple():
+    captions = [
+        ('foo', 1000),
+        ('bar', 501)
+    ]
+    chunks = enricher._create_chunks(captions)
+    assert chunks == [['foo'], ['bar']] # Two chunks
+
+def test_create_chunks_max_size():
+    # Has a maximum size chunk
+    captions = [
+        ('foo', 1000),
+        ('bar', 1500)
+    ]
+    chunks = enricher._create_chunks(captions)
+    assert chunks == [['foo'], ['bar']] # Two chunks
+
+def test_create_chunks_over_max_size():
+    # Chunk over 1500 tokens
+    captions = [('foo', 1501)]
+    with pytest.raises(ValueError):
+        enricher._create_chunks(captions)
+
+# ------------------------------------------------------- GEN PROMPT -------------------------------------------------------
+def test_gen_prompt_empty():
+    # Edge case: empty caption
+    prompt = enricher._gen_prompts([])
+    assert prompt == '\n'
+
+def test_gen_prompt_single():
+    # Single caption
+    prompt = enricher._gen_prompts(['foo'])
+    assert prompt == '\nfoo'
+
+def test_gen_prompt_multiple():
+    # Multiple captions
+    prompt = enricher._gen_prompts(['foo', 'bar'])
+    assert prompt == '\nfoo\nbar'
+
+# ------------------------------------------------------- CHAT COMPLETION -------------------------------------------------------
+# Will not test because interacts with OpenAI API
+# Will leave it to the will of the gods
+
+# ------------------------------------------------------- PARSE COMPLETION -------------------------------------------------------
+def test_parse_completion_empty():
+    # Edge case: empty caption
+    completion = 'Foo.\nBar.\nBaz.\n'
+    parsed_completion = enricher._parse_completion(completion)
+    assert parsed_completion == [
+        {'text': 'Foo.', 'start': None, 'position': 0},
+        {'text': 'Bar.', 'start': None, 'position': 5},
+        {'text': 'Baz.', 'start': None, 'position': 10}
+    ]
+
+# ------------------------------------------------------- Interpolator -------------------------------------------------------
+positions = [(0, 0.00), (5, 6.00), (10, 9.00)]
+interpolator = enricher.Interpolator(positions)
+
+def test_interpolate_defined():
+    # Edge case: interpolationpoints matches curve points
+    assert interpolator.interpolate(0) == 0.00
+    assert interpolator.interpolate(5) == 6.00
+    assert interpolator.interpolate(10) == 9.00
+
+def test_interpolate_between():
+    # Edge case: interpolationpoints does not match curve points
+    assert interpolator.interpolate(1) == 1.20
+    assert interpolator.interpolate(2) == 2.40
+    assert interpolator.interpolate(3) == 3.60
+    assert interpolator.interpolate(4) == 4.80
+
+    assert interpolator.interpolate(6) == 6.6
+    assert interpolator.interpolate(7) == 7.2
+    assert interpolator.interpolate(8) == 7.8
+    assert interpolator.interpolate(9) == 8.4
+
+def test_interpolate_outside():
+    # Edge case: interpolationpoints outside curve points
+    assert interpolator.interpolate(-1) == -1.2
+    assert interpolator.interpolate(11) == 10.6
