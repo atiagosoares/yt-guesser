f437da1b1977208b4365e2287a87979c8b8651ac
atiagosoares <atiagosoares@gmail.com>
2023-04-02 19:28:40 -0300

    Deleted unused files

--- build.sh
+++
@@ -1,8 +0,0 @@
-# Do everything
-rm -rf build/do_everything/*
-pip install -r src/do_everything/requirements.txt -t build/do_everything
-cp src/do_everything/* build/do_everything
-
-# GET videos
-rm -rf build/videos_get/*
-cp src/videos_get/main.py build/videos_get/main.py
