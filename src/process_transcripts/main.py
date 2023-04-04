89f0b50970d8ad8845aa7491f7402a2dd3fc930c
atiagosoares <atiagosoares@gmail.com>
2023-04-03 21:20:29 -0300

    Separated the event processing code into a controller class to make it more testable

--- src/process_transcripts/main.py
+++ src/process_transcripts/main.py
@@ -5,7 +5,6 @@ from enricher import OpenAIChatEnricher
 import db
 import nltk
 
-
 OPENAI_API_KEY_PARAMETER_NAME = os.environ.get('OPENAI_API_KEY_PARAMETER_NAME')
 
 _DB = None
@@ -19,48 +18,66 @@ def get_db():
         _DB = db.DynamoDB(channels_table, videos_table, transcripts_table)
     return _DB
 
+def get_enricher():
+    api_key = _get_parameter_value(OPENAI_API_KEY_PARAMETER_NAME)
+    enricher = OpenAIChatEnricher(api_key)
+    return enricher
+
 def handler(event, context):
 
-    # Unpack s3 data
-    s3_event = event['Records'][0]['s3']
-
-    # Get the video id from the event
-    video_id = s3_event['object']['key'].split('.')[0]
-    # Get the transcript from s3
-    s3 = boto3.resource('s3')
-    obj = s3.Object(s3_event['bucket']['name'], s3_event['object']['key'])
-    transcript = json.loads(obj.get()['Body'].read().decode('utf-8'))
-    
-    # Load openai api key
-    api_key = _get_parameter_value(OPENAI_API_KEY_PARAMETER_NAME)
-    # Build enricher object
-    enricher = OpenAIChatEnricher(api_key)
-    enriched_transcript = enricher.enrich(transcript)
-
-    # Create transcript objects
-    transcript_objects = _create_transcript_objects_from_transcript(enriched_transcript, video_id)
-
-    # Insert transcript objects into the transcripts table
     db = get_db()
-    for transcript_object in transcript_objects:
-        db.transcripts_create(transcript_object)
+    enricher = get_enricher()
 
+    controller = Controller(enricher, db)
 
-def _create_transcript_objects_from_transcript(transcript, video_id):
-    transcript_objects = []
-    for item in transcript:
-        transcript_object = {
-            'video_id': video_id,
-            'start': item['start'],
-            'text': item['text'],
-            'speaker': item.get('speaker', None),
-            'curated': None,
-            'url': f'https://www.youtube.com/watch?v={video_id}&t={item["start"]//1000}'
-        }
-        transcript_objects.append(transcript_object)
-    return transcript_objects
+    for record in event['Records']:
+        s3_event = record['s3']
+        controller.process_event(s3_event)
+
+class Controller():
+
+    def __init__(self, enricher, db):
+        self.enricher = enricher
+        self.db = db
+
+    def process_event(self, event):
+        # Get the video id from the event
+        video_id = event['object']['key'].split('.')[0]
+
+        # Load the transcript
+        transcript = self._load_transcript(event['bucket']['name'], event['object']['key'])
+
+        # Enrich the transcript
+        enriched_transcript = self.enricher.enrich(transcript)
+
+        # Create transcript objects
+        transcript_objects = self._create_transcript_objects_from_transcript(enriched_transcript, video_id)
+
+        # Insert transcript objects into the transcripts table
+        for transcript_object in transcript_objects:
+            self.db.transcripts_create(transcript_object)
+    
+    def _load_transcript(self, bucket_name, key):
+        s3 = boto3.resource('s3')
+        obj = s3.Object(bucket_name, key)
+        transcript = json.loads(obj.get()['Body'].read().decode('utf-8'))
+        return transcript
+
+    def _create_transcript_objects_from_transcript(transcript, video_id):
+        transcript_objects = []
+        for item in transcript:
+            transcript_object = {
+                'video_id': video_id,
+                'start': item['start'],
+                'text': item['text'],
+                'speaker': item.get('speaker', None),
+                'curated': None,
+                'url': f'https://www.youtube.com/watch?v={video_id}&t={item["start"]//1000}'
+            }
+            transcript_objects.append(transcript_object)
+        return transcript_objects
 
 def _get_parameter_value(parameter_name):
-    ssm = boto3.client('ssm')
-    response = ssm.get_parameter(Name=parameter_name, WithDecryption=True)
-    return response['Parameter']['Value']
+        ssm = boto3.client('ssm')
+        response = ssm.get_parameter(Name=parameter_name, WithDecryption=True)
+        return response['Parameter']['Value']
